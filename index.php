be97c629e4010d2d546474f45c55ac6c29490a03
Cal Henderson <cal@iamcal.com>
2011-11-25 16:09:59 -0800

    finalizing the submitted jams stuff

--- index.php
+++ index.php
@@ -10,11 +10,18 @@
 
 	if ($_GET['id']){
 		$id = intval($_GET['id']);
-		$jam = mysql_fetch_array(mysql_query("SELECT * FROM jams WHERE id='$id'"));
+		$jam = mysql_fetch_array(mysql_query("SELECT * FROM jams WHERE id='$id' AND is_deleted=0"));
+
+		if (!$jam['id']){
+			include('head.txt');
+			echo "<h1 style=\"text-align: center; margin-top: 30%; font-size: 40px\">Jam not found :(</h1>";
+			include('foot.txt');
+			exit;
+		}
 	}
 
 	if (!$jam['id']){
-		$jam = mysql_fetch_array(mysql_query("SELECT * FROM jams ORDER BY RAND() LIMIT 1"));
+		$jam = mysql_fetch_array(mysql_query("SELECT * FROM jams WHERE is_approved=1 AND is_deleted=0 ORDER BY RAND() LIMIT 1"));
 		$is_perma = 0;
 	}
 

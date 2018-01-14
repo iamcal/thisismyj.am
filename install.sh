d02d369486330d22cf65e6bafc6b3289b1acd806
Cal Henderson <cal@iamcal.com>
2018-01-14 23:18:32 +0000

    derp - fix path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get install -y imagemagick
 ln -s /var/www/html/thisismyj.am/site.conf /etc/apache2/sites-available/thisismyj.am.conf
-cd /var/www/html/thisismyj.am/thisismyj.am
+cd /var/www/html/thisismyj.am
 (< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-32};echo) > secrets/session_crypto_key
 (< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-40};echo) > secrets/duo_app_key
 a2ensite thisismyj.am
